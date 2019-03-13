03e79697d6048b6ea3cc0b98fc29ac6a2a1daf3f
VukasinVujic <vukasivujic021@gmail.com>
2019-03-13 17:31:50 +0100

    zadatak 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+
+<div>
+<table>
+
+<tr>
+  <th> Sort </th>
+  <th> Name </th>
+  <th> Date of birth  </th>
+</tr>
+
+<tr v-for="animal in animalList" :key="animal.name">
+
+<td> {{animal.sort}} </td>
+<td> {{animal.name}} </td>
+<td> {{animal.dateOfBirth}} </td>
+
+</tr>
+
+</table>
+</div>
+
+
+
+  
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      animalList: [
+        {
+          sort: "pas",
+          name: "lepi",
+          dateOfBirth: "12.02.2000"
+        },
+        {
+          sort: "macka",
+          name: "figaro",
+          dateOfBirth: "12.02.2005"
+        },
+        {
+          sort: "papagaj",
+          name: "ubica",
+          dateOfBirth: "12.02.2010"
+        },
+        {
+          sort: "guster",
+          name: "mekani",
+          dateOfBirth: "12.02.2002"
+        },
+        {
+          sort: "slon",
+          name: "malecki",
+          dateOfBirth: "12.02.2007"
+        },
+      ]
+    };
   }
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
